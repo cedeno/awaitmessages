4b508bfb7267dff833aee85128224ec092ca73bf
Rafhael Cedeno <cedeno@gmail.com>
2021-08-10 17:57:27 +0200

    make getYesNo function

--- index.js
+++ index.js
@@ -8,12 +8,34 @@ client.once('ready', () => {
 });
 
 client.on('messageCreate', async m => {
-	let channel = m.channel;
-	const filter = new_message => new_message.author.id == m.author.id && !new_message.author.bot;
-	let collected = await channel.awaitMessages({ filter, max: 1, time: 60000, errors: ['time'] });
-	let response = collected.first();
-	console.log(`got response ${response}`);
-	channel.send(`got your response ${response}`);
+	if (m.content.startsWith("!delete") && !m.author.bot) {
+		let response = await getYesNo(m, "Are you sure? YES NO");
+		if (response.content == "YES") {
+			// delete stuff
+			await m.channel.send(`Starting deleting. Please wait...`);
+			await deleteEverything();
+			await m.channel.send(`Deletion complete.`);
+		}
+		else {
+			m.channel.send(`Aborting deletion.`);
+		}
+	}
 });
 
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function deleteEverything() {
+	// do stuff
+	await timeout(2000);
+}
+
+async function getYesNo(message, question) {
+	let result_message = await message.channel.send(question);
+	const filter = new_message => new_message.author.id == message.author.id && !new_message.author.bot;
+	let collected = await message.channel.awaitMessages({ filter, max: 1, time: 60000, errors: ['time'] });
+	let response = collected.first();
+	return response;
+}
 client.login(token);
